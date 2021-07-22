d4c0005707739396152b25b58394997a8d536bb2
Sebastian <novas.lobog@konradlorenz.edu.co>
2021-07-22 13:56:42 -0500

    dependecias y componentes

--- prueba-tecnica-metnet/src/app/app.module.ts
+++ prueba-tecnica-metnet/src/app/app.module.ts
@@ -4,13 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**
+ * Angular Material
+ */
+
+ import {MatGridListModule} from '@angular/material/grid-list';
+ import {MatCardModule} from '@angular/material/card';
+ import {MatFormFieldModule} from '@angular/material/form-field';
+ import {MatSelectModule} from '@angular/material/select';
+ import {MatIconModule} from '@angular/material/icon';
+ import {MatTableModule} from '@angular/material/table';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
